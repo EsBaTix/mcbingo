b43067829a3c7330a44e666bed95f84521d374e1
EsBaTix <esbatix+github@posteo.net>
2025-11-06 22:14:09 +0100

    adding new goals list to site

--- bingo.js
+++ bingo.js
@@ -37,6 +37,7 @@ var hoveredSquare;
 //
 // The name is used for display purposes only.
 var VERSIONS = [
+	{ id:"lce", name: "Legacy Console Edition", goals: bingoList_lce, generator: generator_v4, stable: false },
 	{ id:"1", name:"v1 [1.12.2]",		goals: bingoList_v1, generator: generator_v1, stable: true },
 	{ id:"2", name:"v2 [1.12.2]",		goals: bingoList_v2, generator: generator_v2, stable: true },
 	{ id:"3", name:"v3 [1.13.2]", 		goals: bingoList_v3, generator: generator_v2, stable: true },
