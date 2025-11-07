d01b5d5808ec681ba4899839d44997c535661d1c
EsBaTix <esbatix+github@posteo.net>
2025-11-07 17:53:58 +0100

    fixing dolphin typo

--- Goals/goals_v5.js
+++ Goals/goals_v5.js
@@ -591,7 +591,7 @@ var bingoList_v5 = [
 	{name: "Get a Shulker into the Overworld", difficulty: 180, tags: [Action, Combat, End, Overworld]},
 	{name: "Convert a Pig into a Zombified Piglin", difficulty: 200, tags: [Action, Overworld, Storm]},
 	{name: "Tame a Skeleton Horse", difficulty: 200, tags: [Action, Overworld, Storm]},
-	{name: "Remove an Effect from Yourself", difficulty: 8, tooltiptext: "Effects are things like Posion, Dolphion's Grace etc. Waiting for them to naturally run out doesn't count.", reactant: ["UseBuckets"], tags: [Action, Overworld]},
+	{name: "Remove an Effect from Yourself", difficulty: 8, tooltiptext: "Effects are things like Posion, Dolphin's Grace etc. Waiting for them to naturally run out doesn't count.", reactant: ["UseBuckets"], tags: [Action, Overworld]},
 	{name: "Play a Goat Horn", difficulty: 21, antisynergy: ["GoatHorn"], tags: [Action, RareBiome, Overworld]},
 	{name: "Play 4 Different Goat Horns", difficulty: 32, antisynergy: ["GoatHorn"], tags: [Action, RareBiome, Overworld]},
 	{name: "Play 8 Different Goat Horns", difficulty: 150, antisynergy: ["GoatHorn"], tags: [Action, RareBiome, Overworld]},
